c42cf38e27e04009e0f77f0d63aea654e0f19197
Juan Medina <juanjosemedinasepulveda@gmail.com>
2025-05-18 19:29:26 -0500

    Implementacion de contrladores y creacion de rama para completarlos

---
+++ Academia_Sistemas/Academia_Sistemas/Controllers/PagoController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Academia_Sistemas.Clases;
+using Academia_Sistemas.Models;
+using System.Web.Http;
+
+namespace Academia_Sistemas.Controllers
+{
+    [RoutePrefix("api/Pago")]
+    [Authorize]
+    public class PagoController : ApiController
+    {
+
+        [HttpGet]
+        [Route("Consultar")]
+        public Pago Consultar(int IdPago)
+        {
+            clsPago clsPago = new clsPago();
+            return clsPago.Consultar(IdPago); ;
+        }
+
+        [HttpPost]
+        [Route("Insertar")]
+        public string Insertar(Pago pago)
+        {
+            clsPago clsPago = new clsPago();
+            clsPago.pago = pago;
+            return clsPago.Insertar();
+        }
+
+        [HttpPut]
+        [Route("Actualizar")]
+        public string Actualizar(Pago pago)
+        {
+            clsPago clsPago = new clsPago();
+            clsPago.pago = pago;
+            return clsPago.Actualizar();
+        }
+
+        [HttpDelete]
+        [Route("Eliminar")]
+        public string Eliminar(int idpago)
+        {
+            clsPago clsPago = new clsPago();
+            return clsPago.Borrar();
+        }
+    }
+}
---
+++ Academia_Sistemas/Academia_Sistemas/Controllers/SedeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Academia_Sistemas.Models;
+using Academia_Sistemas.Clases;
+
+namespace Academia_Sistemas.Controllers
+{
+    [RoutePrefix("api/Sede")]
+    [Authorize]
+    public class SedeController : ApiController
+    {
+
+        [HttpGet]
+        [Route("Consultar")]
+        public Sede Consultar(int IdSede)
+        {
+            clsSede clsSede = new clsSede();
+            return clsSede.Consultar(IdSede); ;
+        }
+
+        [HttpPost]
+        [Route("Insertar")]
+        public string Insertar(Sede sede)
+        {
+            clsSede clsSede = new clsSede();
+            clsSede.sede = sede;
+            return clsSede.Insertar();
+        }
+
+        [HttpPut]
+        [Route("Actualizar")]
+        public string Actualizar(Sede sede)
+        {
+            clsSede clsSede = new clsSede();
+            clsSede.sede = sede;
+            return clsSede.Actualizar();
+        }
+
+        [HttpDelete]
+        [Route("Eliminar")]
+        public string Eliminar(int idsede)
+        {
+            clsSede clsSede = new clsSede();
+            return clsSede.Borrar();
+        }
+    }
+}
